95f72f5be49d7e99bed2fa5d546057680a6a8562
Janmejaya7326 <152247351+Janmejaya7326@users.noreply.github.com>
2023-11-29 04:22:11 +0000

    file added

---
+++ 01basic/test.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+/*
+1-Java script is a scripting language used to develope web and mobile application efficientely.
+2-We use javascript in both client side as well as server sidelanguage.
+3-We use javascript for developing logics in our code.
+4-Javascript was developed in 1997.
+5-Now a days we used ES6 version of javascript.It was strated on 2015 and now it is latest version of js.
+6-ES6-Echma Script 6.(Valina JavaScript)
+*/
+
