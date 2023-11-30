3a785cb6ec45b89443da7f89cb0d274459e69c1a
Janmejaya7326 <152247351+Janmejaya7326@users.noreply.github.com>
2023-11-30 04:53:18 +0000

    file update

--- 01basic/conversionOperation.js
+++ 01basic/conversionOperation.js
@@ -2,3 +2,6 @@ let scope="33abc"
 console.log(typeof scope);
 let newvar=Number(scope); //for converting data type of a variable use datatype name(variablename)
 console.log(typeof newvar);
+console.log(2%3);
+console.log("1"+2+2);
+console.log(1+2+"2")
